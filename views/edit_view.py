9009390f891bcd9be35d28f65baf8af9789d9c04
Milan Sipos <Sipos.Milan22@gmail.com>
2026-02-26 22:06:50 +0100

    feat:save and delete logic

--- views/edit_view.py
+++ views/edit_view.py
@@ -1,13 +1,16 @@
 
-from PySide6.QtWidgets import (QWidget, QAbstractItemView, QListView, QVBoxLayout, QListWidget, QListWidgetItem, QFileDialog, QLabel, QLineEdit)
+from PySide6.QtWidgets import (QWidget, QAbstractItemView, QListView, QVBoxLayout, QListWidget, QListWidgetItem, QFileDialog, QPushButton)
 from PySide6.QtCore import QSize
 from PySide6.QtGui import QIcon, QPixmap
 from PySide6.QtPdf import QPdfDocument
 from pypdf import PdfReader, PdfWriter
+from PySide6.QtCore import Qt
 
 class EditView(QWidget):
     def __init__(self, pdf_path):
         super().__init__()
+
+        self.pdf_path = pdf_path
 
         self.setWindowTitle("Edit PDF")
         self.resize(1200, 1050)
@@ -27,8 +30,14 @@ class EditView(QWidget):
         self.pages.setDropIndicatorShown(True)
         self.pages.setDragDropMode(QAbstractItemView.DragDropMode.InternalMove)
 
+        self.pages.itemDoubleClicked.connect(self.remove_page)
+
+        self.saveButton = QPushButton("Save new PDF")
+        self.saveButton.clicked.connect(self.export_rearranged_pdf)
+
         layout = QVBoxLayout(self)
         layout.addWidget(self.pages)
+        layout.addWidget(self.saveButton)
 
         self.document = QPdfDocument()
         self.document.load(pdf_path)
@@ -39,10 +48,28 @@ class EditView(QWidget):
             icon = QIcon(pixmap)
 
             item = QListWidgetItem(icon, f"{i+1}")
+
+            item.setData(Qt.ItemDataRole.UserRole, i)
+
             self.pages.addItem(item)
             
+    def export_rearranged_pdf(self):
+        print("asd")
+        savepath, _ = QFileDialog.getSaveFileName(self, "Save Rearranged PDF", "", "PDF Files (*.pdf)")
+
+        reader = PdfReader(self.pdf_path)
+        writer = PdfWriter()
+
+        for i in range(self.pages.count()):
+            item = self.pages.item(i)
+            original_index = item.data(Qt.ItemDataRole.UserRole)
+            writer.add_page(reader.pages[original_index])
+
+        with open(savepath, "wb") as output_file:
+            writer.write(output_file)
+
+        print("file saved to " + savepath)
         
-
-        
-
-
+    def remove_page(self, item):
+        row_index = self.pages.row(item)
+        self.pages.takeItem(row_index)
