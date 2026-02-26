34e63887fcdc80a0f333546f76a3b07373fc97e0
Milan Sipos <Sipos.Milan22@gmail.com>
2026-02-26 15:37:20 +0100

    feat: viewing pdf and refactor into classes

--- pdf.py
+++ pdf.py
@@ -1,32 +1,48 @@
 import sys
 from PySide6.QtWidgets import (QApplication, QWidget, QPushButton, 
-                               QVBoxLayout, QListWidget, QFileDialog)
+                               QVBoxLayout, QListWidget, QFileDialog, QLabel, QLineEdit)
 
+from pdf_viewer import PDFViewerWindow
 
+class MainScreen(QWidget):
+    def __init__(self):
+        super().__init__()
 
+        self.setWindowTitle("PDF Tool Prototype")
+        self.resize(600, 450)
+
+        self.pdf_list = QListWidget()
+        self.load_button = QPushButton("Select PDF File")
+        self.label = QLabel()
+        self.label.setText("asd")
+        self.edit = QLineEdit()
+        self.edit.editingFinished.connect(self.textchange)
+
+        layout = QVBoxLayout(self)
+        layout.addWidget(self.pdf_list)
+        layout.addWidget(self.load_button)
+        layout.addWidget(self.label)
+        layout.addWidget(self.edit)
+
+        self.load_button.clicked.connect(self.open_file_dialog)
+    
+    def open_file_dialog(self):
+        file_name, _ = QFileDialog.getOpenFileName(self, "Open PDF", "", "PDF Files (*.pdf)")
+        
+        self.pdf_list.addItem(file_name)
+
+        view_window = PDFViewerWindow(file_name)
+        view_window.show()
+        print("file showing")
+
+    def textchange(self):
+        print("text was changed")
+    
 def main():
 
-    def open_file_dialog():
-        file_name, _ = QFileDialog.getOpenFileName(window, "Open PDF", "", "PDF Files (*.pdf)")
-        
-        pdf_list.addItem(file_name)
-
-
     app = QApplication(sys.argv)
-    window = QWidget()
 
-    window.setWindowTitle("PDF Tool Prototype")
-    window.resize(600, 450)
-
-    layout = QVBoxLayout(window)
-
-    pdf_list = QListWidget()
-    load_button = QPushButton("Select PDF File")
-
-    layout.addWidget(pdf_list)
-    layout.addWidget(load_button)
-
-    load_button.clicked.connect(open_file_dialog)   
+    window = MainScreen()
 
     window.show()
     sys.exit(app.exec())
---
+++ pdf_viewer.py
@@ -0,0 +1,23 @@
+import sys
+from PySide6.QtWidgets import (QMainWindow, QApplication, QWidget, QPushButton, 
+                               QVBoxLayout, QListWidget, QFileDialog, QLabel, QLineEdit)
+from PySide6.QtPdf import (QPdfDocument)
+from PySide6.QtPdfWidgets import (QPdfView)
+
+class PDFViewerWindow(QMainWindow):
+    def __init__(self, pdf_path:str):
+        super().__init__()
+        self.setWindowTitle("viewing pdf " + pdf_path)
+        self.resize(800, 600)
+
+        central_widget = QWidget()
+        self.setCentralWidget(central_widget)
+        layout = QVBoxLayout(central_widget)
+
+        self.document = QPdfDocument()
+        self.document.load(pdf_path)
+
+        self.view = QPdfView(self)
+        self.view.setDocument(self.document)
+
+        layout.addWidget(self.view)
